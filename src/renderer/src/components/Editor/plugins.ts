584e9a34fa0ba77f71fba92f526ccc1d4357f317
lastwhisper <lastwhisper66@gmail.com>
2026-07-10 12:19:54 +0900

    feat: 组合 ProseMirror 插件

---
+++ src/renderer/src/components/Editor/plugins.ts
@@ -0,0 +1,19 @@
+import { history } from 'prosemirror-history'
+import { keymap } from 'prosemirror-keymap'
+import { baseKeymap } from 'prosemirror-commands'
+import { dropCursor } from 'prosemirror-dropcursor'
+import { gapCursor } from 'prosemirror-gapcursor'
+import type { Plugin } from 'prosemirror-state'
+import { keymapBindings } from './commands'
+import { buildInputRules } from './inputrules'
+
+export function buildPlugins(): Plugin[] {
+  return [
+    buildInputRules(),
+    keymap(keymapBindings),
+    keymap(baseKeymap),
+    dropCursor(),
+    gapCursor(),
+    history()
+  ]
+}
